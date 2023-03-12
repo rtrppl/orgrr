6d4cee81d69aaf0de73c3d6c5c8f19940bade960
rtrppl <63909884+rtrppl@users.noreply.github.com>
2023-03-12 17:53:07 +0100

    Correction of stupid mistake in orgrr-show-backlinks

--- orgrr.el
+++ orgrr.el
@@ -6,59 +6,55 @@
                         line)
       (match-string 2 line))))
 
-
-
 (defun orgrr-show-backlinks ()
   "Show all backlinks to current file."
   (interactive)
   (let ((filename (if (equal major-mode 'dired-mode)
                         default-directory
                       (buffer-file-name))))
-    (pcase (org-collect-keywords '("TITLE"))
-      (`(("TITLE" . ,val))
-         (setq title (car val))))
-(setq backlinks 0)
-    (with-temp-buffer
-      (insert (shell-command-to-string (concat "rg -e '" (file-name-nondirectory filename) "' " org-directory)))
-      (let ((result '())
+     (pcase (org-collect-keywords '("TITLE"))
+       (`(("TITLE" . ,val))
+          (setq title (car val))))
+ (setq backlinks 0)
+     (with-temp-buffer
+       (insert (shell-command-to-string (concat "rg -e '" (file-name-nondirectory filename) "' " org-directory)))
+       (let ((result '())
             (current-entry "")
             (lines (split-string (buffer-string) "\n" t)))
-        (dolist (line lines)
-          (if (string-match "^\\(.*?\\):\\(.*\\)$" line)
-              (progn
-		(setq backlinks (+ backlinks 1))
-                (setq current-entry (match-string 1 line))
-                (setq result (plist-put result current-entry (match-string 2 line))))
-            (setq current-entry (concat current-entry "\n" line)))
+         (dolist (line lines)
+           (if (string-match "^\\(.*?\\):\\(.*\\)$" line)
+               (progn
+ 		(setq backlinks (+ backlinks 1))
+                 (setq current-entry (match-string 1 line))
+                 (setq result (plist-put result current-entry (match-string 2 line))))
+             (setq current-entry (concat current-entry "\n" line)))
           (setq result-list result))))
       (with-current-buffer (get-buffer-create "*Orgrr Backlinks*")
         (let ((inhibit-read-only t))
           (erase-buffer)
-          (insert (concat "\*\[\[file:" filename "\]\[" title "\]\]\*\n\n"))
-          (if (= (- (length result-list) 1) 1)
-              (insert "* 1 Backlink\n\n")
-          (insert (concat "* " (number-to-string backlinks) " Backlinks\n\n")))          
-          (dolist (entry result-list)
-            (when (and (stringp entry)
-                       (not (string= entry filename)))
-              (let ((key entry)
-                    (value (plist-get result-list entry)))
-                (when (stringp value)
-                  (let ((result (orgrr-get-title key))
-			(line-number (orgrr-get-line-number key filename)
-                    (insert (concat "\*\* \[\[file:" key "\]" "\[" result "\]\]:\n\n" value "\n\n"))))))))
-        (display-buffer-in-side-window
-         (current-buffer)
-         '((side . right)
-           (slot . -1)
-           (window-width . 60)))
-(with-current-buffer "*Orgrr Backlinks*"
-      (org-mode)
-      (beginning-of-buffer)))))
+           (insert (concat "\*\[\[file:" filename "\]\[" title "\]\]\*\n\n"))
+           (if (= backlinks 1)
+               (insert "* 1 Backlink\n\n")      
+           (insert (concat "* " (number-to-string backlinks) " Backlinks\n\n")))          
+           (dolist (entry result-list)
+             (when (and (stringp entry)
+                        (not (string= entry filename)))
+               (let ((key entry)
+                     (value (plist-get result-list entry)))
+                 (when (stringp value)
+                   (let ((result (orgrr-get-title key)))
+                     (insert (concat "\*\* \[\[file:" key "\]" "\[" result "\]\]:\n\n" value "\n\n"))))))))
+         (display-buffer-in-side-window
+          (current-buffer)
+          '((side . right)
+            (slot . -1)
+            (window-width . 60)))
+ (with-current-buffer "*Orgrr Backlinks*"
+       (org-mode)
+       (beginning-of-buffer)))))
 
 (defun orgrr-update ()
   "Insert a link to another org-file in org-directory via mini-buffer completion"
-  (interactive)
   (with-temp-buffer
       (insert (shell-command-to-string (concat "rg -l --sortr modified  \"\\#\\+title:\" " org-directory)))
       (let ((result '())
@@ -74,7 +70,6 @@
 
 (defun orgrr-update-test ()
   "Insert a link to another org-file in org-directory via mini-buffer completion"
-  (interactive)
   (setq current-entry "")
   (setq orgrr-files "")
   (with-temp-buffer
@@ -97,8 +92,7 @@
 	    (setq current-entry (buffer-substring (line-beginning-position) (line-end-position)))
 	     (when (string-match "\\(#\\+title:\\|#+TITLE:\\)\\s-*\\(.+\\)" current-entry)
 		    (setq orgrr-titles (cons (match-string 2 current-entry) orgrr-titles)))  
-            (forward-line)))
- (message "orgrr updated!"))
+            (forward-line))))
 
 (defun orgrr-insert ()
   "Insert a link to another org-file in org-directory via mini-buffer completion"
